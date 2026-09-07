4efbc773fdcd816efc85b3b54023e4da9314c5d5
Delmainn <134325371+Delmainn@users.noreply.github.com>
2026-09-07 12:47:57 -0400

    Simplify bootloader application jump using vector table via libopencm3 vector.h

--- bootloader/src/bootloader.c
+++ bootloader/src/bootloader.c
@@ -1,5 +1,6 @@
 #include "common-defines.h"
 #include <libopencm3/stm32/memorymap.h>       // gives us FLASH_BASE so we're not hardcoding 0x08000000 by hand
+#include <libopencm3/cm3/vector.h>       
 
 /**  The bootloader is allotted exactly the first 32KB of flash.
  This number has to agree in THREE other places or the whole scheme breaks:
@@ -15,16 +16,8 @@
 
 
 static void jump_to_main(void) {
-    typedef void (*void_fn)(void);
-
-    
-
-    uint32_t* reset_vector_entry = (uint32_t*)(MAIN_APP_START_ADDRESS + 4U);   //pointer to the reset vector
-    uint32_t* reset_vector = (uint32_t*)(*reset_vector_entry);
-
-    void_fn jump_fn = (void_fn)reset_vector;
-
-    jump_fn();
+    vector_table_t* main_vector_table = (vector_table_t*)(MAIN_APP_START_ADDRESS);
+    main_vector_table->reset();
 }
 
 int main(void) {
